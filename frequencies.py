681d65b296b67e92e3bd834b140686364138edaa
Taha-I <tahaislam1111@gmail.com>
2023-10-09 21:54:03 +0100

    Attempt

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,12 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    for i in items:
+        i = str(i)
+        if i in frequencies:
+            frequencies[i] += 1
+        else:
+            frequencies.setdefault(i,1)
+
     return frequencies
